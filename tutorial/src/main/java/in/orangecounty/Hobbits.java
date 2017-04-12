e8ce5eec3940b9d616c899be8ce5c8c78253b34c
Mithun <mithun.kumar@orangecounty.in>
2017-04-12 17:46:47 +0530

    Adding a file Hobbits.java

---
+++ tutorial/src/main/java/in/orangecounty/Hobbits.java
@@ -0,0 +1,26 @@
+package in.orangecounty;
+
+/**
+ * Created by mithun on 4/12/17.
+ */
+public class Hobbits {
+    String name;
+
+    public static void main(String [] args) {
+        Hobbits[] h = new Hobbits[3];
+        int x = 0;
+        while (x < 3) {
+            h[x] = new Hobbits();
+            h[x].name = "bilbo";
+            if (x == 1) {
+                h[x].name = "frodo";
+            }
+            if (x == 2) {
+                h[x].name = "sam";
+            }
+            System.out.println(h[x].name);
+            x=x+1;
+        }
+    }
+}
+
